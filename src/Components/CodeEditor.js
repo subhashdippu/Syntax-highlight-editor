6c5e3daa26df3715826a5d74314ffe3f11aed382
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-01 16:13:08 +0530

    adding Reactjs highlighter

--- src/Components/CodeEditor.js
+++ src/Components/CodeEditor.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
-
+import 'prismjs/components/prism-jsx.min.js';
 const CodeEditor = () => {
     const [code, setCode] = useState('');
     const [language, setLanguage] = useState('html');
@@ -10,27 +10,27 @@ const CodeEditor = () => {
         setCode(event.target.value);
     };
 
-    const highlightCode = (code) => {
-        return Prism.highlight(code, Prism.languages[language], language);
-    };
-
     const getLanguage = () => {
         switch (language) {
             case 'html':
                 return 'html';
             case 'javascript':
                 return 'javascript';
+            case 'jsx':
+                return 'language-jsx';
             default:
                 return 'html';
         }
+    };
+    const highlightCode = (code) => {
+        return Prism.highlight(code, Prism.languages[language], language);
     };
 
     return (
         <div className='container'>
 
             <div style={{ marginBottom: '1rem' }}>
-                <button
-                    onClick={() => setLanguage('html')}
+                <button onClick={() => setLanguage('html')}
                     style={{
                         marginRight: '0.5rem',
                         padding: '0.5rem 1rem',
@@ -41,8 +41,7 @@ const CodeEditor = () => {
                 >
                     HTML
                 </button>
-                <button
-                    onClick={() => setLanguage('javascript')}
+                <button onClick={() => setLanguage('javascript')}
                     style={{
                         marginRight: '0.5rem',
                         padding: '0.5rem 1rem',
@@ -53,34 +52,39 @@ const CodeEditor = () => {
                 >
                     JavaScript
                 </button>
+                <button onClick={() => setLanguage('jsx')}
+                    style={{
+                        marginRight: '0.5rem',
+                        padding: '0.5rem 1rem',
+                        backgroundColor: language === 'jsx' ? 'gray' : 'transparent',
+                        border: 'none',
+                        cursor: 'pointer',
+                    }}
+                >
+                    ReactJS
+                </button>
             </div>
-            <textarea
-                value={code}
-                onChange={handleCodeChange}
-                style={{
-                    width: '100%',
-                    height: '300px',
-                    opacity: 1,
-                    caretColor: 'black',
-                    border: '1px solid gray',
-                    padding: '10px',
-                    fontSize: '16px',
-                }}
-            />
-            <pre
-                style={{
-                    backgroundColor: 'black',
-                    color: 'white',
-                    padding: '10px',
-                    fontSize: '16px',
-                    overflow: 'auto',
-                    border: '1px solid gray',
-                    height: '300px',
-                    position: 'relative',
-                    whiteSpace: 'pre-wrap',
-                    wordWrap: 'break-word',
-                }}
-            >
+            <textarea value={code} onChange={handleCodeChange} style={{
+                width: '100%',
+                height: '300px',
+                opacity: 1,
+                caretColor: 'black',
+                border: '1px solid gray',
+                padding: '10px',
+                fontSize: '16px',
+            }} />
+            <pre style={{
+                backgroundColor: 'black',
+                color: 'white',
+                padding: '10px',
+                fontSize: '16px',
+                overflow: 'auto',
+                border: '1px solid gray',
+                height: '300px',
+                position: 'relative',
+                whiteSpace: 'pre-wrap',
+                wordWrap: 'break-word',
+            }}>
                 <code
                     className={getLanguage()}
                     dangerouslySetInnerHTML={{ __html: highlightCode(code) }}
